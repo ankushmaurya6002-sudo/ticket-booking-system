d79f58ab3f9aa1701b719818126d64979d7a29d0
Ankush Maurya <ankushmaurya6002@gmail.com>
2026-03-08 21:40:43 +0530

    fix seat click

--- public/script.js
+++ public/script.js
@@ -5,40 +5,47 @@ const rows = ["A","B","C","D"]
 const cols = 6
 
 for(let r of rows){
-    for(let c=1;c<=cols;c++){
+  for(let c=1;c<=cols;c++){
 
-        const seatId = r+c
+    const seatId = r+c
 
-        const seat = document.createElement("div")
-        seat.className="seat"
-        seat.innerText=seatId
+    const seat = document.createElement("div")
+    seat.className="seat"
+    seat.innerText=seatId
 
-        seat.onclick = () => bookSeat(seat,seatId)
+    seat.onclick = () => bookSeat(seat,seatId)
 
-        seatsContainer.appendChild(seat)
+    seatsContainer.appendChild(seat)
 
-    }
+  }
 }
 
 async function bookSeat(element,seatId){
 
+  try{
+
     const response = await fetch("/lock-seat",{
-        method:"POST",
-        headers:{
-            "Content-Type":"application/json"
-        },
-        body:JSON.stringify({
-            seatId:seatId,
-            userId:"user"+Math.floor(Math.random()*10000)
-        })
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify({
+        seatId:seatId,
+        userId:"user"+Math.floor(Math.random()*10000)
+      })
     })
 
     const data = await response.json()
 
-    status.innerText=data.message
+    document.getElementById("status").innerText = data.message
 
     if(data.success){
-        element.classList.add("locked")
+      element.classList.add("locked")
     }
+
+  }catch(err){
+    console.error(err)
+    document.getElementById("status").innerText = "Server error"
+  }
 
 }
