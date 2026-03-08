16d0cc7d12c24e1e730f8baa50f54fd6ea7d65f7
Ankush Maurya <ankushmaurya6002@gmail.com>
2026-03-08 22:00:56 +0530

    final redis tls fix

--- redisClient.js
+++ redisClient.js
@@ -1,14 +1,18 @@
 const { createClient } = require("redis");
 
 const client = createClient({
-  url: process.env.REDIS_URL
+  url: process.env.REDIS_URL,
+  socket: {
+    tls: true,
+    servername: "redis-17434.c62.us-east-1-4.ec2.cloud.redislabs.com"
+  }
 });
 
 client.on("error", (err) => {
   console.error("Redis Client Error:", err);
 });
 
-async function connectRedis(){
+async function connectRedis() {
   await client.connect();
   console.log("Connected to Redis");
 }
