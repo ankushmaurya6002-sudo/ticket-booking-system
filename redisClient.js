01214de0bc2a2b47063a57c942c91f3efbed0ad2
Ankush Maurya <ankushmaurya6002@gmail.com>
2026-03-08 21:16:00 +0530

    fix redis tls connection

--- redisClient.js
+++ redisClient.js
@@ -1,9 +1,21 @@
 const { createClient } = require("redis");
 
 const client = createClient({
-  url: process.env.REDIS_URL
+  url: process.env.REDIS_URL,
+  socket: {
+    tls: true,
+    rejectUnauthorized: false
+  }
 });
 
 client.connect();
+
+client.on("connect", () => {
+  console.log("Connected to Redis");
+});
+
+client.on("error", (err) => {
+  console.log("Redis Error:", err);
+});
 
 module.exports = client;
